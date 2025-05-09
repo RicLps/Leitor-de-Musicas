d4922078a5c0a9e0a26956482d55f6cdb1a93e93
PsychoMan01 <monteiromassimiliano0@gmail.com>
2025-05-09 17:02:38 -0100

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include "funcoes_auxiliares.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> 
 
-int main(){
+printf("\n+===============================+\n");
+printf("| A Tocar: %s\n", m.titulo);
+printf("| Artista: %s\n", m.artista);
+printf("| Duração: %d segundos\n", m.duracao);
+printf("+===============================+\n");
+printf("| p - Parar\n");
+printf("+===============================+\n");
 
-  return 0;
-}
+printf("\n+===============================+\n");
+printf("|      Leitor de Música em C    |\n");
+printf("+===============================+\n");
+printf("| 1. Adicionar música           |\n");
+printf("| 2. Listar músicas             |\n");
+printf("| 3. Tocar música               |\n");
+printf("| 4. Sair                       |\n");
+printf("+===============================+\n");
+printf("Escolha uma opção: ");
+scanf("%d", &opcao);
