dd458d4cf055a230ac0195559cf5548344b1ed42
Ricardo Santos <rickydavidbans27@gmail.com>
2025-05-20 17:30:16 -0100

    Update funcoes_auxiliares.c

--- funcoes_auxiliares.c
+++ funcoes_auxiliares.c
@@ -2,29 +2,25 @@
 #include <stdio.h>
 #include <string.h>
 
-void adicionar(){
- #include "funcoes_auxiliares.h"
-#include <stdio.h>
+void adicionar(Musica lista_musicas[], int *total) { //  o *total armazena a lista de músicas disponíveis;
 
-void adicionar(Musica lista_musicas[ ], int *total) { //  o *total armazena a lista de músicas disponíveis
-
-    if (*total >= MAX_MUSICAS) {
+      if (*total >= MAX_MUSICAS) { // verifica se a lista de musicas está cheia;
         printf("A lista de músicas está cheia.\n");
         return;
     }
 
-Musica musica_nova; // crie uma "variavel" chama musica_nova (dentro do struct Musica) que contem todos os atributos de Musica por exemplo o id.
+Musica musica_nova; // crie uma "Variavel" musica_ nova (dentro do struct Musica) que contem todos os atributos de Musica por exemplo id;
 
 musica_nova.id = *total + 1;  // adiciona o id da musica na lista de musicas (*total) 
 
-printf(“Insira o nome da musica: \n”);
-fgets(musica_nova.nome, sizeof(musica_nova.nome), stdin);
+printf("Insira o nome da musica: \n");
+fgets(musica_nova.nome, TAM_NOME, stdin);
 
-printf("Insira o nome do artista da musica: \n");
-fgets(musica_nova.artista, sizeof(musica_nova.artista), stdin);
+ printf("Insira o nome do artista da musica: \n");
+fgets(musica_nova.artista, TAM_ARTISTA, stdin);
 
 printf("Insira o gênero musical da música que está a inserir: \n");
-fgets(musica_nova.genero, sizeof(musica_nova.genero), stdin);
+fgets(musica_nova.genero, TAM_GENERO, stdin);
 
 printf("Insira a duração da música (em segundos): \n");
 scanf("%d", &musica_nova.duracao);
@@ -34,6 +30,7 @@ lista_musicas[*total] = musica_nova;
 
 printf("Música adicionada na lista de músicas com sucesso! \n");
 }
+
 
 void listar(){
     
