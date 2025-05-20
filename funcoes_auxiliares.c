dd3c031de15ad9f69d8e57d8cb6fb46c0a5607b8
Ricardo Santos <rickydavidbans27@gmail.com>
2025-05-20 19:00:48 -0100

    Update funcoes_auxiliares.c

--- funcoes_auxiliares.c
+++ funcoes_auxiliares.c
@@ -40,7 +40,29 @@ void eliminar(){
    
 }
 
-void tocar_musica(){
-    
+void eliminar(Musica lista_musicas[], int *total) {
+int id = 0;
+int musica_encontrada = 0;
+
+printf("Digite o ID da música que deseja eliminar: \n");
+scanf("%d", &id);
+
+for (int i = 0; i < *total; i++) { // Verifique todas as músicas do programa
+if (lista_musicas[i].id == id) {
+musica_encontrada = 1; // mostra que a musica com o id que inserimos existe e foi encontrado e tambem serve como condição de paragem para que o prog não faça o if(musica_encontrada == 0){
+
+     
+for (int j = i; j < *total - 1; j++) { 
+lista_musicas[j] = lista_musicas[j + 1]; // após a musica ser removida, desloca o musica a seguir da música removida para o id da musica removida
+}
+
+(*total)--; // para descer o número de musicas da lista de musicas
+printf("Música com id %d removida com sucesso!\n", id); // msg que a musica que o utilizador queira remover foi removido
+break;
+}
+}
+if(musica_encontrada == 0){ // caso a musica não for encontrada, ou seja, quando musica_encontrada for igual a zero, mostra msg que a musica que o utilizador quer remover não existe
+printf(“A música com id %d não foi encontrada na lista de músicas.”, id);
+}   
 }
 
