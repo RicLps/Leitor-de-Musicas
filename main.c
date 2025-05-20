67c7399f819b9c29ee67330bec3ef51df606a2d5
PsychoMan01 <monteiromassimiliano0@gmail.com>
2025-05-20 15:58:19 -0100

    Update main.c

--- main.c
+++ main.c
@@ -2,32 +2,78 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#ifdef _WIN32
+    #define LIMPAR_TELA "cls"
+#else
+    #define LIMPAR_TELA "clear"
+#endif
 
-    
+#define MAX_MUSICAS 100
+
 int main() {
     Musica lista[MAX_MUSICAS];
     int total = 0;
+    int opcao, subopcao;
 
     do {
+        system(LIMPAR_TELA);
         printf("\n+===============================+\n");
         printf("|      Leitor de Música em C    |\n");
         printf("+===============================+\n");
-        printf("| 1. Adicionar música           |\n");
-        printf("| 2. Listar músicas             |\n");
-        printf("| 3. Tocar música               |\n");
-        printf("| 4. Eliminar música            |\n");
+        printf("| 1. Gerenciar Músicas          |\n");
+        printf("| 2. Tocar Música               |\n");
         printf("| 0. Sair                       |\n");
         printf("+===============================+\n");
         printf("Escolha uma opção: ");
         scanf("%d", &opcao);
 
         switch (opcao) {
-            case 1: adicionar(); break;
-            case 2: listar(); break;
-            case 3: tocar_musica(); break;
-            case 4: eliminar(); break;
-            case 0: printf("Saindo...\n"); break;
-            default: printf("Opção inválida.\n");
+            case 1:
+                do {
+                    system(LIMPAR_TELA);
+                    printf("\n+===============================+\n");
+                    printf("|        Gerenciar Músicas      |\n");
+                    printf("+===============================+\n");
+                    printf("| 1. Adicionar música           |\n");
+                    printf("| 2. Listar músicas             |\n");
+                    printf("| 3. Eliminar música            |\n");
+                    printf("| 0. Voltar                     |\n");
+                    printf("+===============================+\n");
+                    printf("Escolha uma opção: ");
+                    scanf("%d", &subopcao);
+
+                    switch (subopcao) {
+                        case 1: adicionar(lista, &total); break;
+                        case 2: listar(lista, total); break;
+                        case 3: eliminar(lista, &total); break;
+                        case 0: break;
+                        default: printf("Opção inválida.\n"); break;
+                    }
+
+                    if (subopcao != 0) {
+                        printf("\nPressione Enter para continuar...");
+                        getchar(); getchar();  // Captura Enter
+                    }
+
+                } while (subopcao != 0);
+                break;
+
+            case 2:
+                system(LIMPAR_TELA);
+                tocar_musica(lista, total);
+                printf("\nPressione Enter para continuar...");
+                getchar(); getchar();
+                break;
+
+            case 0:
+                printf("Saindo...\n");
+                break;
+
+            default:
+                printf("Opção inválida.\n");
+                printf("Pressione Enter para continuar...");
+                getchar(); getchar();
+                break;
         }
 
     } while (opcao != 0);
