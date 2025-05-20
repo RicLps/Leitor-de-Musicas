e2593de4c3c2a30b83cbaf454a8faeb769551c14
PsychoMan01 <monteiromassimiliano0@gmail.com>
2025-05-20 15:37:15 -0100

    Update funcoes_auxiliares.h

--- funcoes_auxiliares.h
+++ funcoes_auxiliares.h
@@ -14,7 +14,6 @@ typedef struct {
 void adicionar(Musica lista[], int *total);
 void listar(Musica lista[], int total);
 void eliminar(Musica lista[], int *total, int id);
-void editar(Musica lista[], int total, int id);
 void tocar_musica(Musica lista[], int total, int id);
 
 
