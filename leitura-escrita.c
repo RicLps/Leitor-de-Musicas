c6d98b4efc580bb0d1cffd20ba2561599a23d2cd
Raíssa Monteiro <raissamonteirocoffee@gmail.com>
2025-05-26 12:59:33 -0100

    Add files via upload

---
+++ leitura-escrita.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//leitura
+void leitura(const char *nome){
+	FILE *arquivo = fopen(nome, "r");
+	if(arquivo == NULL){
+		printf("Não foi possível abrir o arquivo\n");
+		return;
+	
+	}
+
+	char conteudo[200];
+	printf("%s:\n", nome);
+	while (fgets(conteudo, sizeof(conteudo), arquivo));
+	printf("%s",conteudo);
+}
+
+	
+
+//Escrever
+void escrever(const char *nome){
+	FILE *arquivo = fopen(nome, "w");
+	if(arquivo == NULL){
+		printf("Não foi possível abrir o arquivo");
+		return;
+}
+
+	fprintf(arquivo, "%s");
+}
+
+
+	int main(){
+	escrever("musica");
+	leitura("musica");
+	return 0;
+	}
+	
