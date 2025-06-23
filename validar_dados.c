8e0bb416a35991cb8ebf2364a09fd5dd9288094c
PsychoMan01 <monteiromassimiliano0@gmail.com>
2025-06-23 14:12:18 -0100

    Update validar_dados.c

--- validar_dados.c
+++ validar_dados.c
@@ -4,24 +4,24 @@
 #include <ctype.h>
 
 int validarDados(const char *entrada) {
+    if (entrada == NULL) return 0;
+    
     int len = strlen(entrada);
-
-    if(len < 3 || len >= TAM_NOME) {
-        printf("Atencao: O nome deve ter entre 3 a %d caracteres\n", TAM_NOME-1);
+    if (len < 3 || len > 50) {
+        printf("Atenção: O nome deve ter entre 3 a 50 caracteres\n");
         return 0;
     }
 
     for (int i = 0; i < len; i++) {
         char c = entrada[i];
-     
-        if(!isalnum(c) && c != ' ' && c != '_' && c != '-' ) {
-            printf("Atencao: Nome invalido, use somente letras, numeros, espaco, underscore e hifens.\n");
+        if (!isalnum(c) && c != ' ' && c != '_' && c != '-' && c != '\'') {
+            printf("Atenção: Caractere inválido encontrado: '%c'\n", c);
             return 0;
         }
     }
 
     if (strspn(entrada, " ") == len) {
-        printf("Atencao: Insira algum caracter valido\n");
+        printf("Atenção: O campo não pode ser vazio\n");
         return 0;
     }
 
