3d13dc2116460be82fb4bf1f85e2098425318394
PsychoMan01 <monteiromassimiliano0@gmail.com>
2025-06-23 12:46:58 -0100

    Update leitura-escrita.c

--- leitura-escrita.c
+++ leitura-escrita.c
@@ -1,40 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void leitura(const char *nome) {
+    FILE *arquivo = fopen(nome, "r");
+    if(arquivo == NULL) {
+        printf("Nao foi possivel abrir o arquivo\n");
+        return;
+    }
 
-//leitura
-void leitura(const char *nome){
-	FILE *arquivo = fopen(nome, "r");
-	if(arquivo == NULL){
-		printf("Nao foi possivel abrir o arquivo\n");
-		return;
-	
-	}
-
-	char conteudo[200];
-	printf("%s:\n", nome);
-	while (fgets(conteudo, sizeof(conteudo), arquivo));
-	printf("%s",conteudo);
+    char conteudo[200];
+    printf("%s:\n", nome);
+    while (fgets(conteudo, sizeof(conteudo), arquivo)) {
+        printf("%s", conteudo);
+    }
+    fclose(arquivo);
 }
 
-	
+void escrever(const char *nome, const char *conteudo) {
+    FILE *arquivo = fopen(nome, "w");
+    if(arquivo == NULL) {
+        printf("Nao foi possivel abrir o arquivo");
+        return;
+    }
 
-//Escrever
-void escrever(const char *nome){
-	FILE *arquivo = fopen(nome, "w");
-	if(arquivo == NULL){
-		printf("Não foi possivel abrir o arquivo");
-		return;
+    fprintf(arquivo, "%s", conteudo);
+    fclose(arquivo);
 }
-
-	fprintf(arquivo, "%s");
-}
-
-
-	// int main(){
-	//	escrever("musica");
-	// leitura("musica");
-	// return 0;
-	// }
-	
-	
