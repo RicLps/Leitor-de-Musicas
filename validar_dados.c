be1b7f15ce643eb87f9f3ee5513517e8035abccc
PsychoMan01 <monteiromassimiliano0@gmail.com>
2025-06-23 13:21:21 -0100

    Update validar_dados.c

--- validar_dados.c
+++ validar_dados.c
@@ -3,32 +3,27 @@
 #include <string.h>
 #include <ctype.h>
 
-//Função que recebe o texto inserido pelo utilizador e verifica se está correto, seguindo alguns parâmetros
-int validarDados(const char *entrada){
-	int len = strlen(entrada);
+int validarDados(const char *entrada) {
+    int len = strlen(entrada);
 
+    if(len < 3 || len >= TAM_NOME) {
+        printf("Atencao: O nome deve ter entre 3 a %d caracteres\n", TAM_NOME-1);
+        return 0;
+    }
 
-	//Verificar a qntd de caracteres
-	if(len < 3 || len > 50){
-		printf("Atencao: O nome deve ter entre 3 a 50 caracteres");
-		return 0;
-	}
+    for (int i = 0; i < len; i++) {
+        char c = entrada[i];
+     
+        if(!isalnum(c) && c != ' ' && c != '_' && c != '-' ) {
+            printf("Atencao: Nome invalido, use somente letras, numeros, espaco, underscore e hifens.\n");
+            return 0;
+        }
+    }
 
-	//Para verificar os carcteres invalidos
-	for (int i = 0; i < len; i++){
-	     char c = entrada[i];
-	 
-	     if(!isalnum(c) && c != ' ' && c != '_' && c != '-' ){
-	     	printf("Atencao: Nome invalido, use soente letras, numeros, espaco, underscore e hifens.");
-	     	return 0;
-	     }
-	}
+    if (strspn(entrada, " ") == len) {
+        printf("Atencao: Insira algum caracter valido\n");
+        return 0;
+    }
 
- 	//Não deixar texto em branco / vazio
-	if (strspn(entrada, " ") == len){
-	   printf("Atencao: Insira algum caracter valido");
-		return 0;
-	}
-
-	return 1;
+    return 1;
 }
